2fbf3a2e457341582bf88b20940d0cdfd1ddd98c
Brendan J. Herger <b@revup.com>
2017-11-01 21:28:34 -0700

    First pass at LinkedLists, without documentation

--- bin/data_structures/linked_list.py
+++ bin/data_structures/linked_list.py
@@ -1,23 +1,87 @@
 class LinkedList(object):
 
     def __init__(self):
-        pass
+        self.first = None
+        self.last = None
 
     def to_list(self):
-        pass
+        # If list has no elements, return empty list
+        if self.first is None:
+            return list()
+        else:
+            next = self.first
+            agg = list()
+            while next is not None:
+                agg.append(next.load)
+                next = next.next_node
+            return agg
 
     def add(self, E):
-        pass
+        new_node = Node(E)
+        # If list has no elements, set first and last
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
 
-    def get_first(self):
-        pass
-
-    def get_last(self):
-        pass
+        # If list already has elements, link to last and update last
+        else:
+            self.last.set_next_node(new_node)
+            self.last = new_node
 
     def remove_first(self):
-        pass
+        # If list has no elements, throw error
+        if self.first is None:
+            raise IndexError('Linked list has no first element to remove')
+
+        # Else remove first element and update pointers
+        else:
+            old_first = self.first
+            new_first = old_first.next_node
+            self.first = new_first
+
+            if new_first is None:
+                self.last = None
+
+            return old_first.load
 
     def remove_last(self):
-        pass
+        # If list has no elements, throw error
+        if self.first is None:
+            raise IndexError('Linked list has no first element to remove')
+
+        # Else remove last element and update pointers
+        else:
+
+            # If there is only one element, return that
+            if self.first == self.last:
+                return self.remove_first()
+
+            # If there is more than one element, find new last and update
+            else:
+                old_last = self.last
+
+                new_last_found = False
+                new_last = self.first
+                while not new_last_found:
+
+                    if new_last.next_node == old_last:
+                        new_last_found = True
+                    else:
+                        new_last = new_last.next_node
+
+                self.last = new_last
+                self.last.set_next_node(None)
+                return old_last.load
+
+
+class Node(object):
+
+    def __init__(self, load):
+
+        self.load = load
+        self.next_node = None
+
+    def set_next_node(self, next_node):
+
+        self.next_node = next_node
 
--- bin/tests.py
+++ bin/tests.py
@@ -5,9 +5,82 @@ from data_structures import linked_list
 
 class LinkedListTests(unittest.TestCase):
 
-    def test(self):
+    def test_empty_list(self):
         ll = linked_list.LinkedList()
         self.assertEquals(ll.to_list(), list())
+
+    def test_single_element_list(self):
+
+        ll = linked_list.LinkedList()
+        test_list = ['First']
+
+        for element in test_list:
+            ll.add(element)
+        self.assertEquals(ll.to_list(), test_list)
+
+    def test_triple_element_list(self):
+
+        ll = linked_list.LinkedList()
+        test_list = ['First', 'Second', 'Third']
+
+        for element in test_list:
+            ll.add(element)
+
+        self.assertEquals(ll.to_list(), test_list)
+
+    def test_last_list(self):
+        ll = linked_list.LinkedList()
+        test_list = ['First', 'Second', 'Third']
+
+        for element in test_list:
+            ll.add(element)
+
+        self.assertEquals(ll.to_list()[-1], ll.last.load)
+
+    def test_remove_first_single(self):
+        ll = linked_list.LinkedList()
+        test_list = ['First']
+
+        for element in test_list:
+            ll.add(element)
+
+        self.assertEquals(ll.remove_first(), test_list[0])
+        self.assertEquals(ll.last, None)
+        self.assertEquals(ll.to_list(), test_list[1:])
+
+
+    def test_remove_first_multiple(self):
+        ll = linked_list.LinkedList()
+        test_list = ['First', 'Second', 'Third']
+
+        for element in test_list:
+            ll.add(element)
+
+        self.assertEquals(ll.remove_first(), test_list[0])
+        self.assertEquals(ll.to_list(), test_list[1:])
+
+    def test_remove_last_single(self):
+        ll = linked_list.LinkedList()
+        test_list = ['First']
+
+        for element in test_list:
+            ll.add(element)
+
+        self.assertEquals(ll.remove_last(), test_list[-1])
+        self.assertEquals(ll.last, None)
+        self.assertEquals(ll.to_list(), test_list[:-1])
+
+    def test_remove_last_multiple(self):
+        ll = linked_list.LinkedList()
+        test_list = ['First', 'Second', 'Third']
+
+        for element in test_list:
+            ll.add(element)
+        last = ll.remove_last()
+        self.assertEquals(last, test_list[-1])
+        self.assertEquals(ll.to_list(), test_list[:-1])
+
+
 
 if __name__ == '__main__':
     unittest.main()
