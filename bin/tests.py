b95a9258f1656f87166b83eb8f323f5a5f3c4afb
Brendan J. Herger <b@revup.com>
2017-11-02 10:58:17 -0700

    First pass at binary trees

--- bin/tests.py
+++ bin/tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from data_structures.binary_tree import BinaryTree
 from data_structures import linked_list
 
 
@@ -80,6 +81,114 @@ class LinkedListTests(unittest.TestCase):
         self.assertEquals(last, test_list[-1])
         self.assertEquals(ll.to_list(), test_list[:-1])
 
+class BinaryTreeTests(unittest.TestCase):
+
+    @staticmethod
+    def generate_no_element_tree():
+        tree = BinaryTree()
+        return tree
+
+    @staticmethod
+    def generate_one_element_tree():
+        tree = BinaryTree()
+        tree.add('banana')
+        return tree
+
+    @staticmethod
+    def generate_two_element_tree():
+        tree = BinaryTree()
+        tree.add('apple')
+        tree.add('banana')
+        return tree
+
+    @staticmethod
+    def generate_five_element_tree():
+        tree = BinaryTree()
+        tree.add('apple')
+        tree.add('banana')
+        tree.add('chocolate')
+        tree.add('durian')
+        tree.add('eclair')
+        return tree
+
+
+    def test_no_element(self):
+        tree = self.generate_no_element_tree()
+
+        self.assertItemsEqual(tree.to_list(), list())
+        self.assertFalse(tree.contains('flour'))
+
+
+    def test_one_element(self):
+        tree = self.generate_one_element_tree()
+
+        self.assertItemsEqual(tree.to_list(), ['banana'])
+        self.assertTrue(tree.contains('banana'))
+        self.assertFalse(tree.contains('flour'))
+
+    def test_two_element_tree(self):
+        tree = self.generate_two_element_tree()
+        self.assertItemsEqual(tree.to_list(), ['apple', 'banana'])
+        self.assertTrue(tree.contains('apple'))
+        self.assertTrue(tree.contains('banana'))
+        self.assertFalse(tree.contains('flour'))
+
+    def test_five_element_tree(self):
+
+        tree = self.generate_five_element_tree()
+        self.assertItemsEqual(tree.to_list(), ['apple', 'banana', 'chocolate', 'durian', 'eclair'])
+        self.assertTrue(tree.contains('apple'))
+        self.assertTrue(tree.contains('banana'))
+        self.assertTrue(tree.contains('chocolate'))
+        self.assertTrue(tree.contains('durian'))
+        self.assertTrue(tree.contains('eclair'))
+        self.assertFalse(tree.contains('flour'))
+
+
+    def test_no_element_remove(self):
+        tree = self.generate_no_element_tree()
+        with self.assertRaises(Exception) as context:
+            tree.remove('flour')
+
+        self.assertTrue('cannot remove' in str(context.exception))
+
+    def test_one_element_remove(self):
+        tree = self.generate_one_element_tree()
+        with self.assertRaises(Exception) as context:
+            tree.remove('flour')
+
+        self.assertTrue('cannot remove' in str(context.exception))
+
+        self.assertTrue(tree.contains('banana'))
+        tree.remove('banana')
+        self.assertFalse(tree.contains('banana'))
+
+    def test_two_element_remove(self):
+        tree = self.generate_two_element_tree()
+
+        with self.assertRaises(Exception) as context:
+            tree.remove('flour')
+
+        self.assertTrue('cannot remove' in str(context.exception))
+
+        self.assertTrue(tree.contains('banana'))
+        tree.remove('banana')
+        self.assertFalse(tree.contains('banana'))
+
+        self.assertItemsEqual(tree.to_list(), ['apple'])
+
+    def test_five_element_remove(self):
+        tree = self.generate_five_element_tree()
+
+        with self.assertRaises(Exception) as context:
+            tree.remove('flour')
+
+        self.assertTrue('cannot remove' in str(context.exception))
+
+        self.assertTrue(tree.contains('banana'))
+        tree.remove('banana')
+        self.assertFalse(tree.contains('banana'))
+        self.assertItemsEqual(tree.to_list(), ['apple', 'chocolate', 'durian', 'eclair'])
 
 
 if __name__ == '__main__':
