a9e4b30bbda8fb66303f9be03890d1f5bcd95f01
Brendan J. Herger <b@revup.com>
2017-11-02 07:53:58 -0700

    Interface for binary tree

---
+++ bin/data_structures/binary_tree.py
@@ -0,0 +1,34 @@
+class BinaryTree(object):
+    # Roughly based on http://www1.cs.columbia.edu/~bert/courses/3134/slides/Lecture7.pdf
+
+    def __init__(self):
+        pass
+
+    def to_list(self):
+        pass
+
+    def add(self):
+        pass
+
+    def contains(self):
+        pass
+
+    def remove(self, element):
+        pass
+
+    def subtree(self, element, traversal):
+        pass
+
+    def re_balance(self):
+        pass
+
+
+class node(object):
+    def __init__(self):
+        pass
+
+    def set_right(self, new_right):
+        pass
+
+    def set_list(self, new_left):
+        pass
