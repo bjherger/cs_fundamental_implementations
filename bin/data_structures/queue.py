340fd9d5440d257e09a04e8471c6b2265880ec5f
Brendan J. Herger <b@revup.com>
2017-11-02 11:09:13 -0700

    Interface for queue

---
+++ bin/data_structures/queue.py
@@ -0,0 +1,24 @@
+class Queue(object):
+
+    # Simple first in, first out (FIFO) queue, based on the Java Queue API: https://docs.oracle.com/javase/7/docs/api/java/util/Queue.html
+
+    def __init__(self):
+        pass
+
+    def add(self):
+        pass
+
+    def element(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def offer(self):
+        pass
+
+    def poll(self):
+        pass
+
+    def remove(self):
+        pass
