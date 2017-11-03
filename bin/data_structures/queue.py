0582f0751169830447686d7e43aa3d870daf7d4e
Herger <brendan.herger@capitalone.com>
2017-11-03 10:34:58 -0700

    Interface for queue

---
+++ bin/data_structures/queue.py
@@ -0,0 +1,21 @@
+class Queue(object):
+
+    def __init__(self):
+        pass
+
+    def add(self, load):
+        pass
+
+    def to_list(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def remove(self):
+        pass
+
+class QueueNode(object):
+
+    def __init__(self):
+        pass
