276efba9d07202125f3e768986ee9375898510ac
Brendan J. Herger <b@revup.com>
2017-11-05 15:11:24 -0800

    Interface for min_heap

---
+++ bin/data_structures/min_heap.py
@@ -0,0 +1,16 @@
+class min_head(object):
+
+    def __init__(self):
+        pass
+
+    def add(self, elem):
+        pass
+
+    def to_list(self):
+        pass
+
+    def peek(self):
+        pass
+
+    def remove(self):
+        pass
