f15754c999a6001d6cf599c940875d260d41b70b
Herger <brendan.herger@capitalone.com>
2017-11-14 16:43:49 -0800

    Interface for matrixgraph

---
+++ bin/data_structures/matrix_graph.py
@@ -0,0 +1,20 @@
+class MatrixGraph(object):
+
+    def __init__(self, max_nodes):
+        pass
+
+    def add_node(self, data):
+        pass
+
+    def to_node_list(self):
+        pass
+
+    def to_edge_list(self):
+        pass
+
+    def add_edge(self, parent, child):
+        pass
+
+    def get_node_index(self, data):
+        pass
+
